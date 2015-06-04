ce638674889fc9b02b2055e1f4323db6f4b2ca64
Mikael Midtgaard <mikaelmidt@gmail.com>
2015-06-04 14:00:21 +0200

    logic working now

--- gridUtil.js
+++ gridUtil.js
@@ -6,13 +6,13 @@ var GridUtil = {
     calculateNextGenerationForGrid: function(grid){
         var newGrid = [[]];
 
-        for(var j = 0; j < grid.length; j++) {
-            newGrid[j] = [];
-            for(var i = 0; i < grid[i].length; i++) {
+        for(var i = 0; i < grid.length; i++) {
+            newGrid[i] = [];
+            for(var j = 0; j < grid[i].length; j++) {
                 var neighbours = 0;
                 //left
-                if(i > 0){
-                    if (grid[i - 1][j]){
+                if(j > 0){
+                    if (grid[i][j - 1]){
                         neighbours++;
                     }
                 }
@@ -23,38 +23,38 @@ var GridUtil = {
                     }
                 }
                 //up
-                if(j > 0){
-                    if(grid[i][j - 1]){
+                if(i > 0){
+                    if(grid[i - 1][j]){
                         neighbours++;
                     }
                 }
                 //up right
-                if(j > 0 && i < grid[i].length - 1){
-                    if(grid[i + 1][j-1]){
+                if(i > 0 && j < grid.length - 1){
+                    if(grid[i - 1][j + 1]){
                         neighbours++;
                     }
                 }
                 //right
-                if(i < grid[i].length - 1){
-                    if(grid[i + 1][j]){
+                if(j < grid.length - 1){
+                    if(grid[i][j + 1]){
                         neighbours++
                     }
                 }
                 //down right
-                if(i < grid[i].length - 1 && j < grid.length - 1){
+                if(j < grid.length - 1 && i < grid[i].length - 1){
                     if(grid[i + 1][j + 1]){
                         neighbours++;
                     }
                 }
                 //down
-                if(j < grid.length - 1){
-                    if(grid[i][j + 1]){
+                if(i < grid[i].length - 1){
+                    if(grid[i + 1][j]){
                         neighbours++;
                     }
                 }
                 //down left
-                if(i > 0 && j < grid.length - 1){
-                    if(grid[i - 1][j + 1]){
+                if(j > 0 && i < grid[i].length - 1){
+                    if(grid[i + 1][j - 1]){
                         neighbours++;
                     }
                 }
